ee96c4333a02f609093a4bb859b47db81be03736
Poxios <poxios0310@gmail.com>
2023-01-01 21:25:48 +0900

    working on snake

--- 3190. snake.py
+++ 3190. snake.py
@@ -7,6 +7,8 @@ board = [0]*(n*n)
 # apple
 for i in range(int(input())):
     x, y = map(int, input().split())
+    x -= 1
+    y -= 1
     board[x*n+y] = 1
 
 # rotate
@@ -16,7 +18,7 @@ for _ in range(int(input())):
     rotate_timing[int(temp[0])] = temp[1]
 
 time = 0
-direction = 0  # up right down left
+direction = 1  # up right down left
 pos = (0, 0)
 dx = [-1, 0, 1, 0]
 dy = [0, 1, 0, -1]
@@ -31,17 +33,25 @@ while True:
     my_body.append(x*n+y)
     # 방향 회전을 해야할 타이밍인지 확인, 방향 수정
     if time in rotate_timing:
+        print('rotate!!', time, (x, y))
         if rotate_timing[time] == 'L':  # 좌회전
-            direction = direction-1 % 4
+            direction = (direction-1) % 4
         else:  # 우회전
-            direction = direction+1 % 4
+            direction = (direction+1) % 4
 
-        # 다음 칸이 벽이거나 내 몸이면 끝
-        if not (0 <= (nx := x+dx[direction]) < n) or not (0 <= (ny := y+dy[direction]) < n) or board[(p := nx*n+ny)] == 2:
-            break
-        # 다음 칸이 사과이면 꼬리 보존, 다음 칸이 공기면 꼬리 삭제
-        if board[p] == 0:
-            board[my_body.popleft()] = 0
-        # 머리 움직임
-        pos = nx, ny
+    # 다음 칸이 벽이거나 내 몸이면 끝
+    if (not 0 <= (nx := x+dx[direction]) < n) or (not 0 <= (ny := y+dy[direction]) < n) or board[(p := nx*n+ny)] == 2:
+        print('end! next is:', (nx, ny), direction, board[p])
+        print(my_body)
+        break
+    # 다음 칸이 사과이면 꼬리 보존, 다음 칸이 공기면 꼬리 삭제
+    if board[p] == 0:
+        board[my_body.popleft()] = 0
+
+    # DEBUG
+    if board[p] == 1:
+        print('got apple!!', nx, ny)
+    # 머리 움직임
+    pos = (nx, ny)
+    # print(len(my_body))
 print(time)
