2e37f3cec579e68f15662c748465a90bd3108552
Poxios <poxios0310@gmail.com>
2023-01-05 17:29:14 +0900

    baby shark correct

--- 16236. baby shark.py
+++ 16236. baby shark.py
@@ -6,6 +6,7 @@ n = int(input())
 board = []
 shark_pos = (0, 0)
 shark_size = 2
+shark_exp = 0
 fish = []
 time = 0
 
@@ -13,58 +14,63 @@ dx, dy = [1, -1, 0, 0], [0, 0, 1, -1]
 
 for x in range(n):
     for y, v in enumerate(map(int, input().rstrip().split())):
-        board.append(y)
+        board.append(v)
         if v == 9:  # baby shark
             shark_pos = (x, y)
-        elif v != 0:  # fish / {index: size}
+        elif v != 0:
             fish.append((x, y))
+fish.reverse()  # 가장 위에 있는 물고기가 제일 마지막에 계산되어야 한다.
 
 
-# (상어와 물고기의 거리, 도착했을 때 몸집)
-def bfs(x, y, size, tx, ty) -> tuple:
-    q = deque([(x, y, size, 0)])
+def bfs(x, y, tx, ty, size) -> int:
+    q = deque([(x, y)])
     visited = [0]*(n*n)
     while q:
-        x, y, size, exp = q.popleft()
+        x, y = q.popleft()
         for i in range(4):
             nx, ny = x+dx[i], y+dy[i]
-            # 벽이거나 이미 방문했으면 넘긴다.
-            if (not 0 <= nx < n) or (not 0 <= ny < n) or visited[(np := nx*n+ny)] != 0:
+            np = nx*n+ny
+            if nx == tx and ny == ty:  # 끝
+                return visited[x*n+y]+1
+            # 다음자리가 벽이거나 이미 방문했거나 물고기인데 나보다 크거나 작으면 넘긴다.
+            if (not 0 <= nx < n) or (not 0 <= ny < n) or visited[np] != 0 or (1 <= (fs := board[np]) <= 6 and (size < fs or size > fs)):
                 continue
-            if nx == tx and ny == ty:
-                # 끝!
-                # 해당 물고기 먹고 거리, 사이즈 리턴
-                pass
-            # 물고기면 확인한다.
-            if 1 <= board[np] <= 6:
-                # 지나갈 수 없다.
-                if size < board[np]:
-                    continue
 
-                # 크기가 같으면 지나갈 수 있는데 먹을 수는 없다
-                if size == board[np]:
-                    q.append((nx, ny, size, exp))
-                # 먹으면서 지나갈 수 있다.
-                else:
-                    fish.remove((nx, ny))  # FIXME
-                    exp += 1
-                    if exp == size:
-                        size += 1
-                        exp = 0
-                    q.append((nx, ny, size, exp))
-                # 거리 입력
-                visited[np] = visited[x*n+y]+1
-            # 그냥 공기면 그냥 간다.
-            else:
-                q.append((nx, ny, size, exp))
-                visited[np] = visited[x*n+y]+1
+            # 다음으로 나아간다.
+            q.append((nx, ny))
+            visited[np] = visited[x*n+y]+1
     return -1
 
 
 while fish:
-    # 이동할 물고기를 타겟팅한다.
-    min_distance_fish = sys.maxsize
-    for fp in fish:
-        min_distance_fish = min(min_distance_fish, bfs(
-            shark_pos[0], shark_pos[1], 2, fp[0], fp[1]))
-    print(min_distance_fish)
+    # 먹을 수 있는 물고기 중 가장 가까운 물고기를 타겟팅한다.
+    # 가장 가까운 물고기가 많다면 가장 좌표가 작은 물고기를 타겟팅.
+    # 이미 좌표가 내림차순이기때문에 그냥 순서대로 하면 가장 첫번째꺼가 된다.
+    closest_fish_pos = -1
+    closest_fish_distance = sys.maxsize
+    for fp in [f for f in fish if board[f[0]*n+f[1]] < shark_size]:
+        if (temp := bfs(shark_pos[0], shark_pos[1], fp[0], fp[1], shark_size)) != -1 and temp <= closest_fish_distance:
+            closest_fish_pos = fp
+            closest_fish_distance = temp
+    # 먹을 수 있는 물고기가 있다면
+    if closest_fish_pos != -1:
+        # print('eat!!', closest_fish_pos)
+        # 먹고
+        fx, fy = closest_fish_pos
+        fish.remove((fx, fy))
+        board[fx*n+fy] = 0
+        # 경험치 업
+        shark_exp += 1
+        # 레벨업도 한다
+        if shark_exp == shark_size:
+            shark_exp = 0
+            shark_size += 1
+
+        # 시간 반영하고
+        time += closest_fish_distance
+        # 현재 위치 갱신한다
+        shark_pos = (fx, fy)
+    else:  # 먹을 수 있는 물고기가 없다면
+        print(time)
+        exit(0)
+print(time)
